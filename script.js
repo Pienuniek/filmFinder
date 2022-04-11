8a9afce39dcd8d0baad504b28c769d0d28be40bc
Pien Willemse <pienuniek@gmail.com>
2022-04-11 12:49:09 +0200

    Worked further on a solution to get the radiobuttons to perform properly until now still no satisfactory result. i did add the eventlisteners but I dont know if it is correct. I can fill and empty the movieGrid with the all movies and I can filter the right movies from it. I cant however get the radiobuttons to do their job of filtering and filling the grid with only the filtered movies.

--- script.js
+++ script.js
@@ -1,54 +1,106 @@
-const newestRadioBtn = document.getElementById("newest");
-const avengerRadioBtn = document.getElementById("Avenger");
-const xMenRadioBtn = document.getElementById("X-Men");
-const princessRadioBtn= document.getElementById("Princess");
-const batmanRadioBtn = document.getElementById("Batman");
+const radioBtns = document.getElementsByName("moviechoice");
 const movieGrid = document.querySelector(".movieGridContainer");
-const searchPara = ["Avenger", "X-Men", "Princess", "Batman"];
+let filteredMovies = [];
+let newestMovies = [];
 
 //making moviecards
-const generateCardTemplate = (movie)=>{
-    const html = 
-    `<div class="movieCard">
+
+const generateCardTemplate = (movie) => {
+  const html = `<div class="movieCard">
            <a href="https://www.imdb.com/title/${movie.imdbID}/">
                <img src="${movie.poster}" alt="${movie.title}">
            </a>
        </div>
      `;
-     movieGrid.innerHTML += html;
- }
+  movieGrid.innerHTML += html;
+};
 
 //creating the filled grid
-movies.forEach(movie => {
+
+const placeMovies = () => {
+    if (filteredMovies === []){
+        newestMovies.forEach((movie) => {
+            generateCardTemplate(movie);
+        })
+    }else {
+  filteredMovies.forEach((movie) => {
     generateCardTemplate(movie);
-});
+  });
+};
+};
+// empty the grid
 
-//the sorting eventlisteners
-newestRadioBtn.addEventListener('click', ()=>{});
-avengerRadioBtn.addEventListener('click', (movies)=> {
-   movies.filter(movie => {
-      if(!movie.title.includes("Avenger")){
-          movie.classList.add(hidden)
-      } else {movie.classList.remove(hidden)}
-   })
-});
-
-xMenRadioBtn.addEventListener('click', ()=>{});
-princessRadioBtn.addEventListener('click',()=>{});
-batmanRadioBtn.addEventListener('click', ()=>{});
-
-//sorting function
-function moviesFilter(movie){
-    movies.filter(movie.title.includes(item.value))}
-const newestMovies = movies.filter(movie => movie.year >= "2014");
-
-console.log(moviesFilter);
-console.log(newestMovies);
+const deleteMovies = () => movieGrid.innerHTML = "";
+  
 
 
+// individual filters for each radiobutton.value
 
+// const newestMovies = movies.filter(movie => movie.year >= "2014");
+// const batmanMovies = movies.filter(movie => movie.title.includes("Batman"));
+// const avengerMovies = movies.filter(movie => movie.title.includes("Avengers"));
+// const xMenMovies = movies.filter(movie => movie.title.includes("X-Men"));
+// const princessMovies = movies.filter(movie => movie.title.includes//("Princess"));
 
+// console.log(princessMovies);
+// console.log(xMenMovies);
+// console.log(avengerMovies);
+// console.log (batmanMovies)
+// console.log(newestMovies);
 
+//filters to make the groups
 
+function filterMovies(wordInMovieTitle) {
+    deleteMovies();
+    filteredMovies = movies.filter((movie) => {
+            return movie.Title.includes(wordInMovieTitle);
+        });
 
+    placeMovies();
+  }
+function filterNewestMovies() {
+    deleteMovies();
+    newestMovies = movies.filter((movie) => {
+            return movie.Year >= 2014;
+        });
+    placeMovies();
+    }
+//eventListeners
 
+function addEventListeners() {
+    const radioButtons = document.getElementsByName("film-filter");
+
+    radioButtons.forEach((radioButton) => {
+        radioButton.addEventListener("click", handleOnChangeEvent);
+    });
+    console.log(e.target.value);
+}
+
+function handleOnChangeEvent(event) {
+    switch (event.target.value) {
+        case "newest":
+            deleteMovies();
+            newestMovies("2014");
+            break;
+        case "avenger":
+            filterMovies("Avenger");
+            break;
+        case "xmen":
+            filterMovies("X-Men");
+            break;
+        case "princess":
+            filterMovies("Princess");
+            break;
+        case "batman":
+            filterMovies("Batman");
+            break;
+        default:
+            console.log("geen idee welke film");
+            break;
+    }
+}
+
+// initial value, all movies in the grid
+
+filteredMovies = movies;
+placeMovies();
